c2721b82ee9caab79a93aceb66f9c7286793e295
Team City <info@exira.com>
2016-01-11 20:09:38 +0000

    Bump version to 1.3.4

--- src/AssemblyInfo.fs
+++ src/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("ebs-backup")>]
 [<assembly: AssemblyProductAttribute("Exira.EbsBackup")>]
 [<assembly: AssemblyDescriptionAttribute("Exira.EbsBackup is a console application which backs up EBS volumes on a rotating schedule")>]
-[<assembly: AssemblyVersionAttribute("1.2.3")>]
-[<assembly: AssemblyFileVersionAttribute("1.2.3")>]
-[<assembly: AssemblyMetadataAttribute("githash","26c2d4222ee2d798017710c7482ab6ca984145af")>]
+[<assembly: AssemblyVersionAttribute("1.3.4")>]
+[<assembly: AssemblyFileVersionAttribute("1.3.4")>]
+[<assembly: AssemblyMetadataAttribute("githash","562c35d05aaf431e09b89ab353515b0b3f03dce2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.2.3"
+    let [<Literal>] Version = "1.3.4"
